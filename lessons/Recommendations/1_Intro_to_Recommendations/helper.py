26cc31648a281eee6826c5787710d8ea97f33158
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-02-19 00:49:38 -0700

    collaborative filtering

--- lessons/Recommendations/1_Intro_to_Recommendations/helper.py
+++ lessons/Recommendations/1_Intro_to_Recommendations/helper.py
@@ -4,5 +4,5 @@ def test_eucl(x, y):
     return np.linalg.norm(x - y)
     
 
-def test_manhat(x, y):
+def test_manhat(x,y):
     return sum(abs(e - s) for s,e in zip(x, y))
--- lessons/Recommendations/1_Intro_to_Recommendations/tests.py
+++ lessons/Recommendations/1_Intro_to_Recommendations/tests.py
@@ -115,7 +115,7 @@ def test_recs(sol_dict):
     c = "there were too many ratings to get a stable metric"
     d = 'user based collaborative filtering'
     e = "euclidean distance and pearson's correlation coefficient"
-    f = "manhatten distance and euclidean distance"
+    f = "manhattan distance and euclidean distance"
     g = "spearman's correlation and euclidean distance"
     h = "the spread in some ratings was zero"
     i = 'content based recommendation'
@@ -220,7 +220,7 @@ def sim_6_sol(sol_dict):
 def test_recs(sol_dict):
     sol_dict1 = {
         'The type of recommendation system implemented here was a ...': 'user based collaborative filtering',
-        'The two methods used to estimate similarity were: ': "euclidean distance and pearson's correlation coefficient",
+        'The two methods used to estimate user similarity were: ': "euclidean distance and pearson's correlation coefficient",
         'There was an issue with using the correlation coefficient.  What was it?': 'the spread in some ratings was zero'
     }
     if sol_dict1 == sol_dict:
@@ -253,5 +253,3 @@ def test_recs2(sol_dict2):
         for k, v in sol_dict2.items():
             if sol_dict1[k] != sol_dict[k]:
                 print("Oops! Your answer to: {} doesn't look quite right.".format(k))
-
-          
